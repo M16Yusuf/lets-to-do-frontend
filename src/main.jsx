1fd948794ce103bbb71b3534a3df91785ecae40c
M16yusuf <yusufsmd58@gmail.com>
2025-10-25 06:11:07 +0700

    feat: wrap App component with TodoProvider and BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./page/App.jsx";
+import { TodoProvider } from "./contexts/todo/todoContext.jsx";
+import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <TodoProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </TodoProvider>
   </StrictMode>
 );
